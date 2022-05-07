8abec395792b0d89808070e9fad6bbd9a47faaef
Pho Vu <phovu0703@gmail.com>
2022-05-07 16:45:18 -0400

    Initial Commit

---
+++ Count Backwards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Count Backwards
+//
+//  Created by Hemin Vu on 5/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Count Backwards/Count_BackwardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Count_BackwardsApp.swift
+//  Count Backwards
+//
+//  Created by Hemin Vu on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Count_BackwardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
