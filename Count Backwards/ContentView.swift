b34bbf88c5321c09ca6f70533a047fc3323a4c9d
Pho (Tran An) Vu / Haemin <phovu0703@gmail.com>
2022-05-08 02:01:49 -0400

    Update ContentView.swift

--- Count Backwards/ContentView.swift
+++ Count Backwards/ContentView.swift
@@ -1,16 +1,46 @@
 //
 //  ContentView.swift
-//  Count Backwards
+//  "Count Backwards" on Xcode utilizing UI Kit
+//  App structure: Count down from 10 -> 0
+//  Start (pink): start counting down
+//  Reset (orange): go back to the original start (10)
 //
-//  Created by Hemin Vu on 5/7/22.
+//  Created by @Pho Vu on 5/7/22.
+//  Inspired by YT Channel "Code Palace".
+//
 //
 
-import SwiftUI
+import SwiftUI // import Swift user interface framework
 
 struct ContentView: View {
+    @State var countDownTimer = 10 // starting point
+    @State var timerRunning = true
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(countDownTimer)")
+                .onReceive(timer) { _ in
+                    if countDownTimer > 0 && timerRunning {
+                        countDownTimer -= 1
+                    } else {
+                        timerRunning = false
+                    }
+                }
+                .font(.system(size: 80, weight: .bold))
+                .opacity (0.80)
+            
+            
+            HStack(spacing: 100) { // distance between 2 buttons "Start" & "Reset"
+                Button("Hit Start!") { // name the activation button
+                    timerRunning = true
+                } . foregroundColor(.pink) // set the color to pink
+                Button("Reset?") { //name the restart button
+                    countDownTimer = 10
+                } .foregroundColor(.orange) // set the color to orange
+            }
+        }
     }
 }
 
